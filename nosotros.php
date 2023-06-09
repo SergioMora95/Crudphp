d0971b30180f550f1a30c347963acaa16c873479
SergioMora95 <136117139+SergioMora95@users.noreply.github.com>
2023-06-09 13:46:28 -0500

    Add files via upload

---
+++ nosotros.php
@@ -0,0 +1,15 @@
+<?php include("template/heather.php");?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1>
+    <p class="lead">Esta es una pagina de practica de php</p>
+    <hr class="my-2">
+    <p>More info</p>
+    <p class="lead">
+        <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+    </p>
+</div>
+
+
+
+<?php include  ("template/footer.php"); ?>
---
+++ productos.php
@@ -0,0 +1,73 @@
+<?php include("template/heather.php");?>
+
+<div class="col-md-3">
+    
+
+
+    <div class="card">
+    
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+    
+
+
+    <div class="card">
+    
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+    
+
+
+    <div class="card">
+    
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+
+    </div>
+
+</div>
+
+<div class="col-md-3">
+    
+
+
+    <div class="card">
+    
+    <img class="card-img-top" src="holder.js/100x180/" alt="">
+    
+    <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <a name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
+    </div>
+
+    </div>
+
+</div>
+
+
+
+<?php include  ("template/footer.php"); ?>
